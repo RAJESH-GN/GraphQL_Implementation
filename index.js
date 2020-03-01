5a08c6c7d1e7763b069988568e82ccd32103d38a
RAJESH-GN <rajeshgn821@gmail.com>
2020-03-01 13:41:07 +0530

    basic Graphql Setup

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const {buildSchema} = require("graphql");
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.get("/",(req,res,next) =>{
-    res.send("Hello world");
-})
+app.use('/graphql',graphqlHttp({
+    schema:buildSchema(`
+        type RootQuery  {
+            events: [String!]!
+        }
+
+        type RootMutations  {
+            createEvent(name:String):String
+        }
+    schema{
+            query:RootQuery
+            mutation: RootMutations
+        }
+    `),
+    rootValue:{
+        events :() =>{
+            return ["Round night","Cooking","All Night Coding"]
+        },
+        createEvent :(args) =>{
+            return args.name
+        }
+    },
+    graphiql:true
+}))
 app.listen(3000);
